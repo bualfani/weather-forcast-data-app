eea047cb4fd4839819f604bc04d45c9b1bfaa9a2
bahati <bulozealfani@gmail.com>
2023-03-08 10:49:41 -0600

    fixed error code

--- backend.py
+++ backend.py
@@ -3,8 +3,8 @@ import requests
 api_key = "cb30fc9a023abb0b29a499da5d26be92"
 
 
-def get_data(location, days, kind):
-    url = f"api.openweathermap.org/data/2.5/forecast?q={location}&appid={api_key}"
+def get_data(location, days):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?zip={location}&appid={api_key}"
     response = requests.get(url)
     data = response.json()
     filter_data = data['list']
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import plotly.express as px
 from backend import get_data
 
 st.title('Weather Forecast')
-location = st.text_input("Location: ")
+location = st.text_input("Zip Code: ")
 days = st.slider('Forecast by Days', min_value=1, max_value=5,
                  help="Select the number of forecast days")
 
@@ -11,15 +11,22 @@ option = st.selectbox("Select Data to View", ('Graph', 'Sky'))
 
 st.subheader(f'{option} for the next {days} days in {location}')
 
-if location:
-    filter_data = get_data(location, days)
+try:
+    if location:
+        filter_data = get_data(location, days)
 
-    if option == 'Graph':
-        temperature = [dict['main']['temp'] for dict in filter_data]
-        dates = [dict['dt_txt'] for dict in filter_data]
-        figure = px.line(x=dates, y=temperature, labels={'x': 'Date', 'y': 'Temperature'})
-        st.plotly_chart(figure)
+        if option == 'Graph':
+            temperature = [(dict['main']['temp']* (9/5) + 32) / 10 for dict in filter_data]
+            dates = [dict['dt_txt'] for dict in filter_data]
+            figure = px.line(x=dates, y=temperature, labels={'x': 'Date', 'y': 'Temperature(f)'})
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        sky_condition = [dict['weather'][0]['main'] for dict in filter_data]
-        st.image()
+        if option == "Sky":
+            images = {"Clear": "sky_images/clear.png", "Clouds": "sky_images/cloud.png",
+                      "Rain": "sky_images/rain.png", "Snow": "sky_images/snow.png"}
+            sky_condition = [dict['weather'][0]['main'] for dict in filter_data]
+            image_path = [images[condition] for condition in sky_condition]
+            st.image(image_path, width=115)
+
+except KeyError:
+    st.write("Please Enter a Valid Zip Code")
